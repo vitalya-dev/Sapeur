f22f832200b4de6e04a1486d0d5642df4db6381e
vitalya-dev <vitalya.dev@gmail.com>
2021-08-28 09:37:21 +0300

    Work On...

--- Main.gd
+++ Main.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var SIZE = Vector2(10, 10)
+var MINES = 10
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,12 +12,14 @@ var SIZE = Vector2(10, 10)
 func _ready():
 	$Game.rect_size = Vector2(0, 0)
 	make_tiles()
+	distribute_mines()
+	set_neighbors()
 	adjust_window_size();
 	switch_full_screen();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print($Game.rect_size);
+	pass
 
 func _input(ev):
 	if ev.is_action_pressed("ui_cancel"):
@@ -28,9 +31,45 @@ func make_tiles():
 		for x in range(SIZE.x):
 			var tile = preload('res://Tile.tscn').instance()
 			tile.rect_position = Vector2(x,y);
+			tile.connect("open", self, '_on_tile_open', [Vector2(x, y)])
 			$Game/Field.add_child(tile);
-	
 			
+func distribute_mines():
+	# Distribute mines
+	var mines_count = MINES
+	while mines_count > 0:
+		var minepos = randi() % int(SIZE.x * SIZE.y)
+		if not $Game/Field.get_child(minepos).mine:
+			$Game/Field.get_child(minepos).mine = true
+			mines_count -= 1	
+
+func set_neighbors():
+	for y in range(SIZE.y):
+		for x in range(SIZE.x):
+			var mines = 0
+			var pos = Vector2(x, y)
+			for neighbor in get_neighbors(pos):
+				if get_tile(neighbor).mine:
+					mines += 1
+			get_tile(pos).mines_around = mines
+
+func get_tile(pos):
+	return $Game/Field.get_child(pos.x + (pos.y * SIZE.x))
+
+func get_neighbors(pos):
+	var neighbors = []
+	for delta_x in [-1, 0, 1]:
+		for delta_y in [-1, 0, 1]:
+			var neighbor = Vector2(pos.x + delta_x, pos.y + delta_y)
+			if neighbor.x < 0 or neighbor.x >= SIZE.x:
+				continue
+			if neighbor.y < 0 or neighbor.y >= SIZE.y:
+				continue
+			if neighbor.x == pos.x and neighbor.y == pos.y:
+				continue
+			neighbors.append(neighbor)
+	return neighbors;
+				
 func adjust_window_size():
 	yield(get_tree(), "idle_frame")
 	OS.set_window_size($Game.rect_size)
@@ -39,3 +78,8 @@ func adjust_window_size():
 func switch_full_screen():
 	yield(get_tree(), "idle_frame")
 	OS.window_fullscreen = true
+
+func _on_tile_open(pos):
+	if get_tile(pos).mines_around == 0:
+		for neighbor in get_neighbors(pos):
+			get_tile(neighbor).open()
--- Tile.gd
+++ Tile.gd
@@ -5,23 +5,51 @@ extends Button
 # var a = 2
 # var b = "text"
 
-var flagged = false setget _set_flagged
+var mine = false 
+var mines_around = 0
+
+signal open
+
+enum {
+	NORMAL,
+	OPEN,
+	FLAGGED
+}
+
+var state = NORMAL;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _input(ev):
 	if is_hovered() and ev is InputEventMouseButton:
 		if ev.button_index == 2 and ev.pressed:
-			set('flagged', !flagged)
-			
-			
-func _set_flagged(value):
-	flagged = value
-	if flagged:
-		set_text("F")
-	else:
-		set_text("")
+			flag()
+		if ev.button_index == 1 and ev.pressed:
+			open()
+
+func flag():
+	match state:
+		NORMAL:
+			set_text("F")
+			state = FLAGGED
+		FLAGGED:
+			set_text("")
+			state = NORMAL
+								
+func open():
+	match state:
+		FLAGGED, NORMAL:
+			state = OPEN
+			if self.mine:
+				set_text("X")
+			else:
+				set_text(str(mines_around))
+			emit_signal("open")
+	
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
