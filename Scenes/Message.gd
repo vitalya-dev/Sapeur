25b77f2aa2ca2fcb7d2c8544146d3e103bf7e07b
vitalya-dev <vitalya.dev@gmail.com>
2021-09-02 08:28:16 +0300

    Avatars support.

--- Scenes/Message.gd
+++ Scenes/Message.gd
@@ -5,13 +5,16 @@ extends Panel
 # var a = 2
 # var b = "text"
 export var messages: PoolStringArray
-export var avatar: Texture
+export var avatar_1: Texture
+export var avatar_2: Texture
+export var avatar_3: Texture
+export var avatar_4: Texture
+
 var current_message = 0
 var frame_delta = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Avatar/Picture.texture = avatar
 	if messages.size() > 0:
 		show_current_message()	
 
@@ -28,7 +31,16 @@ func _on_Button_pressed():
 		hide()
 
 func show_current_message():
-	$Text.text = messages[current_message]
+	match messages[current_message][0]:
+		"@":
+			$Avatar/Picture.texture = avatar_1
+		"#":
+			$Avatar/Picture.texture = avatar_2
+		"$":
+			$Avatar/Picture.texture = avatar_3
+		"%":
+			$Avatar/Picture.texture = avatar_4
+	$Text.text = messages[current_message].right(1)
 	$Text.percent_visible = 0;		
 	while $Text.percent_visible < 1:
 		$Text.percent_visible += frame_delta
