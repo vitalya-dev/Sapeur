914b892d8fee79396733613a0f216713c3523688
vitalya-dev <vitalya.dev@gmail.com>
2021-09-03 08:58:13 +0300

    Add States To Game.

--- Scenes/Field.gd
+++ Scenes/Field.gd
@@ -7,78 +7,116 @@ export var mines : int
 # var a = 2
 # var b = "text"
 
+var state = "INIT"
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Panel.rect_size = Vector2(0, 0)
-	make_tiles()
-	distribute_mines()
-	set_neighbors()
-	#adjust_window_size();
-	switch_full_screen();
+func init():
+    $Panel.rect_size = Vector2(0, 0)
+    make_tiles()
+    distribute_mines()
+    set_neighbors()
+    #adjust_window_size();
+    switch_full_screen();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    match state:
+        "INIT":
+            init()
+            state = "GAME"
+        "GAME":
+            pass
+        "WIN":
+            pass
+        "FAIL":
+            pass
+        
+            
 
 func _input(ev):
-	if ev.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+    if ev.is_action_pressed("ui_cancel"):
+        get_tree().quit()
 
 func make_tiles():
-	$Panel/Grid.set_columns(size.x)
-	for y in range(size.y):
-		for x in range(size.x):
-			var tile = preload('res://Scenes/Tile.tscn').instance()
-			tile.connect("open", self, '_on_tile_open', [Vector2(x, y)])
-			$Panel/Grid.add_child(tile);
-			
+    $Panel/Grid.set_columns(size.x)
+    for y in range(size.y):
+        for x in range(size.x):
+            var tile = preload('res://Scenes/Tile.tscn').instance()
+            tile.connect("open", self, '_on_tile_open', [Vector2(x, y)])
+            $Panel/Grid.add_child(tile);
+            
 func distribute_mines():
-	randomize()
-	var mines_count = mines
-	while mines_count > 0:
-		var minepos = randi() % int(size.x * size.y)
-		if not $Panel/Grid.get_child(minepos).mine:
-			$Panel/Grid.get_child(minepos).mine = true
-			mines_count -= 1	
+    randomize()
+    var mines_count = mines
+    while mines_count > 0:
+        var minepos = randi() % int(size.x * size.y)
+        if not $Panel/Grid.get_child(minepos).mine:
+            $Panel/Grid.get_child(minepos).mine = true
+            mines_count -= 1	
 
 func set_neighbors():
-	for y in range(size.y):
-		for x in range(size.x):
-			var mines_around = 0
-			var pos = Vector2(x, y)
-			for neighbor in get_neighbors(pos):
-				if get_tile(neighbor).mine:
-					mines_around += 1
-			get_tile(pos).mines_around = mines_around
+    for y in range(size.y):
+        for x in range(size.x):
+            var mines_around = 0
+            var pos = Vector2(x, y)
+            for neighbor in get_neighbors(pos):
+                if get_tile(neighbor).mine:
+                    mines_around += 1
+            get_tile(pos).mines_around = mines_around
 
 func get_tile(pos):
-	return $Panel/Grid.get_child(pos.x + (pos.y * size.x))
+    return $Panel/Grid.get_child(pos.x + (pos.y * size.x))
 
 func get_neighbors(pos):
-	var neighbors = []
-	for delta_x in [-1, 0, 1]:
-		for delta_y in [-1, 0, 1]:
-			var neighbor = Vector2(pos.x + delta_x, pos.y + delta_y)
-			if neighbor.x < 0 or neighbor.x >= size.x:
-				continue
-			if neighbor.y < 0 or neighbor.y >= size.y:
-				continue
-			if neighbor.x == pos.x and neighbor.y == pos.y:
-				continue
-			neighbors.append(neighbor)
-	return neighbors;
-				
+    var neighbors = []
+    for delta_x in [-1, 0, 1]:
+        for delta_y in [-1, 0, 1]:
+            var neighbor = Vector2(pos.x + delta_x, pos.y + delta_y)
+            if neighbor.x < 0 or neighbor.x >= size.x:
+                continue
+            if neighbor.y < 0 or neighbor.y >= size.y:
+                continue
+            if neighbor.x == pos.x and neighbor.y == pos.y:
+                continue
+            neighbors.append(neighbor)
+    return neighbors;
+                
 func adjust_window_size():
-	yield(get_tree(), "idle_frame")
-	OS.set_window_size($Panel.rect_size)
-	
-	
+    yield(get_tree(), "idle_frame")
+    OS.set_window_size($Panel.rect_size)
+    
+    
 func switch_full_screen():
-	yield(get_tree(), "idle_frame")
-	OS.window_fullscreen = true
+    yield(get_tree(), "idle_frame")
+    OS.window_fullscreen = true
 
 func _on_tile_open(pos):
-	if get_tile(pos).mines_around == 0:
-		for neighbor in get_neighbors(pos):
-			get_tile(neighbor).open()
+    if check_fail():
+        state = "FAIL"
+    elif check_win():
+        state = "WIN"
+    elif get_tile(pos).mines_around == 0:
+        print("open other")
+        for neighbor in get_neighbors(pos):
+            get_tile(neighbor).open()
+
+
+func check_fail():
+    for y in range(size.y):
+        for x in range(size.x):
+            var pos = Vector2(x, y)
+            if get_tile(pos).state == "OPEN" and get_tile(pos).mine:
+                return true
+    return false
+
+func check_win():
+    var opened_tiles = 0
+    var flagged_tiles = 0
+    for y in range(size.y):
+        for x in range(size.x):
+            var pos = Vector2(x, y)
+            if get_tile(pos).state == "OPEN":
+                opened_tiles += 1
+            elif get_tile(pos).state == "FLAGGED":
+                flagged_tiles += 1
+    return opened_tiles == size.x * size.y - mines and flagged_tiles == mines
--- Scenes/Tile.gd
+++ Scenes/Tile.gd
@@ -9,68 +9,62 @@ var mine = false
 var mines_around = 0
 
 const text_color = [
-	Color("0000ff"),
-	Color("008000"),
-	Color("ff0000"),
-	Color("000080"),
-	Color("800000"),
-	Color("008080"),
-	Color("000000"),
-	Color("808080")
+    Color("0000ff"),
+    Color("008000"),
+    Color("ff0000"),
+    Color("000080"),
+    Color("800000"),
+    Color("008080"),
+    Color("000000"),
+    Color("808080")
 ]
-	
+    
 signal open
 
-enum {
-	NORMAL,
-	OPEN,
-	FLAGGED
-}
-
-var state = NORMAL;
+var state = "NORMAL";
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+    pass
 
 func _input(ev):
-	if is_hovered() and ev is InputEventMouseButton:
-		if ev.button_index == 2 and ev.pressed:
-			flag()
-		if ev.button_index == 1 and ev.pressed:
-			open()
+    if is_hovered() and ev is InputEventMouseButton:
+        if ev.button_index == 2 and ev.pressed:
+            flag()
+        if ev.button_index == 1 and ev.pressed:
+            open()
 
 func flag():
-	match state:
-		NORMAL:
-			$Text.set_text("F")
-			state = FLAGGED
-		FLAGGED:
-			$Text.set_text("")
-			state = NORMAL
-								
+    match state:
+        "NORMAL":
+            $Text.set_text("F")
+            state = "FLAGGED"
+        "FLAGGED":
+            $Text.set_text("")
+            state = "NORMAL"
+                                
 func open():
-	match state:
-		FLAGGED, NORMAL:
-			state = OPEN
-			if self.mine:
-				$Text.set_text("X")
-			elif mines_around > 0:
-				$Text.set_text(str(mines_around))
-				$Text.set("custom_colors/font_color", text_color[mines_around - 1])
-			else:
-				$Text.set_text("")
-			set_texture(preload("res://Assets/tile_open.png"))
-			emit_signal("open")
-	
+    match state:
+        "FLAGGED", "NORMAL":
+            state = "OPEN"
+            if self.mine:
+                $Text.set_text("X")
+            elif mines_around > 0:
+                $Text.set_text(str(mines_around))
+                $Text.set("custom_colors/font_color", text_color[mines_around - 1])
+            else:
+                $Text.set_text("")
+            set_texture(preload("res://Assets/tile_open.png"))
+            emit_signal("open")
+    
 func set_texture(texture):
-	set_normal_texture(texture)
-	set_hover_texture(texture)
-	set_pressed_texture(texture)
-	set_focused_texture(texture)	
-		
+    set_normal_texture(texture)
+    set_hover_texture(texture)
+    set_pressed_texture(texture)
+    set_focused_texture(texture)	
+        
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
