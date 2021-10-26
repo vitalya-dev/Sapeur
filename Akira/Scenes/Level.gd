80d1ea532ba8497bb98bfd67b5173d1e66749e0b
Vitalya <vitalya.dev@gmail.com>
2021-10-26 07:39:03 +0300

    Working On Message.

--- Akira/Scenes/Level.gd
+++ Akira/Scenes/Level.gd
@@ -8,7 +8,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect_field() 
+	connect_field()
+	_show_tutorial_message(0)
 
 
 func connect_field():
@@ -25,6 +26,12 @@ func _on_tile_demine(tile):
 	$DemineSFX.stop()
 	$DemineSFX.play()
 
+func _show_tutorial_message(step):
+	var message = preload('res://Scenes/Message.tscn').instance()
+	message.messages = ["@Добро пожаловать в симуляцию сержант."]
+	message.avatar_1 = preload("res://Assets/Graphics/Avatars/avatar_doctor.png")
+	message.avatar_2 = preload("res://Assets/Graphics/Avatars/avatar_sergeant.png")
+	add_child(message, true);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
