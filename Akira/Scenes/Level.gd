f2b618a1267e2908e070add0247b4c72461c98fc
Vitalya <vitalya.dev@gmail.com>
2021-10-26 23:19:11 +0300

    Working On Tutorial.

--- Akira/Scenes/Level.gd
+++ Akira/Scenes/Level.gd
@@ -27,11 +27,25 @@ func _on_tile_demine(tile):
 	$DemineSFX.play()
 
 func _show_tutorial_message(step):
-	var message = preload('res://Scenes/Message.tscn').instance()
-	message.messages = ["@Добро пожаловать в симуляцию сержант."]
-	message.avatar_1 = preload("res://Assets/Graphics/Avatars/avatar_doctor.png")
-	message.avatar_2 = preload("res://Assets/Graphics/Avatars/avatar_sergeant.png")
-	add_child(message, true);
+	var message_window = preload('res://Scenes/MessageWindow.tscn').instance()
+	message_window.get_node("Message").messages = [
+		"@Добро пожаловать на полигон, сержант.",
+		"@Пространство поделено на ячейки.",
+		"@Есть ячейки в которых установлена взрывчатка.",
+		"@Твоя работа - найти эти ячейки и обезвредить.",
+		"@Ячейка в которой нет взрывчатки покажет тебе количество соседних ячеек в которых есть взрывчатка.",
+		"@Используя эту информацию даже такой идиот как ты сможет все сделать правильно.",
+		"@Используй левую кнопку мыши что бы открыть ячейку, используй правую кнопку что бы обезвредить ячейку.",
+		"@Любая ошибка недопустима.",
+		"@Обезвредишь ячейку в которой нет взрывчатки - будет взрыв.",
+		"@Откроешь ячейку в которой есть взрывчатка - будет взрыв.",
+		"@Ну? Чего ты ждешь?",
+		"@Вперед!"
+	]
+	message_window.get_node("Message").avatar_1 = preload("res://Assets/Graphics/Avatars/avatar_doctor.png")
+	message_window.get_node("Message").avatar_2 = preload("res://Assets/Graphics/Avatars/avatar_sergeant.png")
+	add_child(message_window, true);
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
