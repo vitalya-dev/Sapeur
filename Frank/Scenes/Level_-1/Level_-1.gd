da3cb6e9598dc1486546687994e410746c424aa6
U-DESKTOP-HOU9BEQ\Vitalya <vitalya.dev@gmail.com>
2022-02-10 09:10:25 +0300

    Music In Menu.

--- Frank/Scenes/Level_-1/Level_-1.gd
+++ Frank/Scenes/Level_-1/Level_-1.gd
@@ -7,10 +7,18 @@ extends Control
 
 signal complete()
 
+
+
+var music_1 = preload("res://Assets/Sounds/Emotions [1213095934].mp3")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$New.connect("pressed", self, "emit_signal", ["complete"])
 	$Exit.connect("pressed", get_node("/root/Game"), "quit_game")
+	################################################################
+	$Music.stream = music_1
+	$Music.play()
+	$Music.fade_in()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
